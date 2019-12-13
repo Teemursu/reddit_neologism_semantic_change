3befbc5f69c9eaff5e66eff383262bbdd4b1e978
Teemu Pöyhönen <teme.polhonen@gmail.com>
2019-12-13 19:59:21 +0200

    Added some documentation

--- process_sentences.py
+++ process_sentences.py
@@ -5,22 +5,22 @@ from nltk.corpus import stopwords
 stopwords = set(stopwords.words('english'))
 
 def process_file(file):
-    data = []
-    comments=[]
+    '''
+    1) Convert sentences into a list of lowercase tokens, ignoring tokens that are too short or too long.
+    2) Filter out deleted/removed comments, empty lines and stop words
+    '''
+    comments = []
     for sent in file:
-        data.append(simple_preprocess(sent))
-    for review in data:
-        wr = []
-        for word in review:
-            if word not in stopwords:
-                wr.append(word)
-            comments.append(wr)
+        comments.append(simple_preprocess(sent))
+    get_rid_of_these = {'http', 'com', 'https', 'html'}
+    comments = [x for x in comments if x != ['deleted'] and x != 'removed']
+    comments[:] = [[word for word in sub if word not in get_rid_of_these] for sub in comments]
+    comments[:] = [[word for word in sub if word not in stopwords] for sub in comments]
     comments = [x for x in comments if x]
-    comments = [x for x in comments if x != ['deleted']]
 
-    for i in range(5):
+    print("... Showing a random comment...")
+    for i in range(1):
         print(random.choice(comments))
-
     return comments
 
 """
--- write_data.py
+++ write_data.py
@@ -1,19 +1,36 @@
 import bz2file
 import json
 import os
-import process_data
 import process_sentences
-from gensim.utils import simple_preprocess
 
 
 def get_sentences(year, month):
-    DATA_DIR = os.path.join("D:\\", "reddit", "reddit_data", str(year))
-    filename = "RC_" + str(year) + "-" + str(month) + ".bz2"
+    '''
+    Read the JSON data files and
+    return a list of all the 'body' values (comments)
+    for that year and month
+    '''
+
     sentences = []
 
-    print(DATA_DIR + "\\" + filename)
-    print("Reading the file '" + DATA_DIR + "\\" + filename)
+    """
+    Define our directory for the Reddit corpus
+    that currently exists on an external hard drive.
 
+    The "reddit_data" directory contains subdirectories
+    which are named as years during which the comments have been made.
+    """
+    DATA_DIR = os.path.join("D:\\", "reddit", "reddit_data", str(year))
+
+    """"
+    Each folder for the year contains a compressed JSON (.bz2) file
+    that has all the comments posted that month. E.g. "RC_2008-06.bz2"
+    """
+    filename = "RC_" + str(year) + "-" + str(month) + ".bz2"
+
+    print("Let's look at " + DATA_DIR + "\\" + filename + "...")
+    print()
+    print("...Reading the file '" + filename+"'...")
     with bz2file.open(DATA_DIR + "\\" + filename, 'r',
                         compresslevel=9,
                         encoding=None,
@@ -23,30 +40,41 @@ def get_sentences(year, month):
             data = json.loads(line)
             sentences.append(data['body'])
         f.close()
-    print("Successfully imported data from the file '" + filename+"'.")
+    print("...Successfully imported data from the file '" + filename+"'.")
+    print()
     return sentences
 
 
 def write_data(year, month, comments):
-    print("Writing data for " + year + "/" + month + " sentences")
+    '''
+    Write a text file in which each line represents a sentence/comment.
+    '''
+    print("Writing data for comments made in " + year + "/" + month +"...")
 
-    with open("comment_data/data" + year + "-" + month + ".txt", "w", encoding='utf-8') as json_f:
+    with open("comment_data/comments" + "-" + year + "-" + month + ".txt", "w", encoding='utf-8') as json_f:
         json_f.write("\n".join([' '.join(map(str, item)) for item in comments]))
         json_f.close()
-        #json.dump(sentences, json_f)
-        #json_f.write("\n")
+        print("...Comment data file successfully created!")
+        print()
 
-
+'''
+A list of years and months lets us loop through the data folders 
+and specify which file to write the comments to.
+'''
 years = ['2006', '2007', '2008', '2009', '2010', '2011',
          '2012', '2013', '2014', '2015', '2016', '2017']
 months = ['01', '02', '03', '04', '05', '06',
           '07', '08', '09', '10', '11', '12']
 
-test_years = ['2009']
+test_years = ['2008', '2009', '2010', '2011']
 test_months = ['01', '02', '12']
 
-for year in test_years:
-    for month in test_months:
+'''
+For each year, go through each month. For each of those months,
+get the data, process the data and write it into a file that Word2Vec accepts.
+'''
+for year in years:
+    for month in months:
         sentences = get_sentences(year, month)
         comments = process_sentences.process_file(sentences)
         write_data(year, month, comments)
