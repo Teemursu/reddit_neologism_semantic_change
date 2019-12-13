b0a6cf195b0de5e9a24da0eb030127a028476dad
Teemu Pöyhönen <teme.polhonen@gmail.com>
2019-12-13 20:02:39 +0200

    Added some documentation

--- create_compass.py
+++ create_compass.py
@@ -1,8 +1,17 @@
 import glob
 
-read_files = glob.glob("comment_data/*.txt")
 
-with open("TWEC_master/examples/training/compass.txt", "wb") as outfile:
-    for f in read_files:
-        with open(f, "rb") as infile:
-            outfile.write(infile.read())
+def get_compass():
+    '''
+    The TEWC requires a compass file
+    which contains all of the processed text data
+    '''
+    read_files = glob.glob("comment_data/*.txt")
+
+    with open("TWEC_master/examples/training/compass.txt", "wb") as outfile:
+        for f in read_files:
+            with open(f, "rb") as infile:
+                outfile.write(infile.read())
+
+
+get_compass()
